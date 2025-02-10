9a2cae82af0e4706863b853aab89cfc5fbd632a7
yassineelmiri <yassireelhima@gmail.com>
2025-02-10 15:10:09 +0100

    Ajout de App.js, fichier principal qui contient la logique d'affichage et les composants principaux de l'application

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider, useSelector } from "react-redux";
+import store from "./redux/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ScanScreen from "./screens/ScanScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+import StatisticsScreen from "./screens/StatisticsScreen";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const MainNavigator = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      {!isAuthenticated ? (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      ) : (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Scan" component={ScanScreen} />
+          <Stack.Screen name="Statistics" component={StatisticsScreen} />
+          <Stack.Screen name="Products" component={ProductListScreen} />
+        </>
+      )}
+    </Stack.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
