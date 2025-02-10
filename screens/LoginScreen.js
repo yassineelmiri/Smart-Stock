e43ddb23bb657a7491b5e4a16dda9b790983911e
yassineelmiri <yassireelhima@gmail.com>
2025-02-10 21:13:24 +0100

    Mise à jour de LoginScreen.js : amélioration de l'interface utilisateur et gestion des erreurs de connexion

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,13 +4,13 @@ import { loginUser } from "../redux/authSlice";
 import { View, Text, TextInput, Button, ActivityIndicator, ImageBackground, TouchableOpacity } from "react-native";
 
 const LoginScreen = ({ navigation }) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [secretKey, setSecretKey] = useState("");
     const dispatch = useDispatch();
     const { loading, error, isAuthenticated } = useSelector(state => state.auth);
 
     const handleLogin = () => {
-        dispatch(loginUser({ username, password }));
+        
+        dispatch(loginUser({ secretKey }));
     };
 
     useEffect(() => {
@@ -29,27 +29,10 @@ const LoginScreen = ({ navigation }) => {
         >
             <View style={{ flex: 1, justifyContent: "center", padding: 20, backgroundColor: 'rgba(0, 0, 0, 0.85)' }}>
                 <Text style={{ fontSize: 24, marginBottom: 20, color: 'white' }}>Connexion</Text>
-                
                 <TextInput
-                    placeholder="Nom d'utilisateur"
-                    value={username}
-                    onChangeText={setUsername}
-                    style={{
-                        borderWidth: 1,
-                        padding: 12,
-                        marginBottom: 15,
-                        borderColor: '#fff',
-                        color: '#fff',
-                        backgroundColor: '#444',
-                        borderRadius: 8,
-                    }}
-                    placeholderTextColor="#ccc"
-                />
-                
-                <TextInput
-                    placeholder="Mot de passe"
-                    value={password}
-                    onChangeText={setPassword}
+                    placeholder="Secret Key"
+                    value={secretKey}
+                    onChangeText={setSecretKey}
                     secureTextEntry
                     style={{
                         borderWidth: 1,
